956cc2d210825578d17e8fef86264d2f10d3edc6
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-09-03 16:43:03 +0700

    Add IPinfo

--- RunGroopWebApp/Controllers/HomeController.cs
+++ RunGroopWebApp/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.ViewModels;
+using System.Net;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace RunGroopWebApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IClubRepository _clubRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,IClubRepository clubRepository)
     {
         _logger = logger;
+        _clubRepository = clubRepository;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var ipInfo = new IPinfo();
+        var homeViewModel = new HomeViewModel();
+        try
+        {
+            string url = "https://ipinfo.io?token=5903b52f8611f1";
+            var info = new WebClient().DownloadString(url);
+            ipInfo = JsonConvert.DeserializeObject<IPinfo>(info);
+            RegionInfo myRegionInfo = new RegionInfo(ipInfo.Country);
+            ipInfo.Country = myRegionInfo.EnglishName;
+            homeViewModel.City = ipInfo.City;
+            homeViewModel.State = ipInfo.Region;
+            if(homeViewModel.City != null)
+            {
+                homeViewModel.Clubs = await _clubRepository.GetClubsByCity(homeViewModel.City);
+            }
+            else
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
+        }
+        catch(Exception ex)
+        {
+            homeViewModel.Clubs = null;
+        }
+        return View(homeViewModel);
     }
 
     public IActionResult Privacy()
---
+++ RunGroopWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.ViewModels
+{
+	public class HomeViewModel
+	{
+		public IEnumerable<Club> Clubs { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+	}
+}
+
